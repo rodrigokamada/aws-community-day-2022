ac328f90eeb34d7ad4d560e0077ace99f794a30d
Rodrigo Kamada <rodrigo@kamada.com.br>
2022-11-05 18:07:49 -0300

    Fix.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  cognito: {
+    userPoolId: 'us-east-1_kjpmKW94b',
+    userPoolWebClientId: 'mpvd12q039hqgib2bd1ddl2am',
+  }
 };
